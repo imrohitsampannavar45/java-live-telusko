df35e007e66aed37a8cc0c7b7727e4b771c36911
iamrohitsampannavar45 <imrohitsampannavar@gmail.com>
2024-11-09 10:46:46 +0530

    objects in java

---
+++ live-10am/Example.java
@@ -0,0 +1,10 @@
+public class Example{
+    public static void main(String[] args){
+
+        Person p = new Person();
+        System.out.println(p.loc);
+        // System.out.println("Hello World");
+
+        p.displayData();
+    }
+}
---
+++ live-10am/Person.java
@@ -0,0 +1,15 @@
+public class Person{
+    String name = "Rohit";
+    int age = 23;
+    String loc =  "Bengaluru";
+
+
+
+
+    public void displayData(){
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(loc);
+
+    }
+}
